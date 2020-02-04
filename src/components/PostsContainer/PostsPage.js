874599725dcb45402f024b2ba5d4c407d294f14e
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-02-04 23:43:27 +0800

    attempted mvp tasks

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -5,12 +5,13 @@ import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
 const PostsPage = (props) => {
+  // console.log(props.data);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
       {props.data.map((post, index) => {
         <Post post={post} key={index} />
-        console.log(post, index);
+        // console.log(post, index);
       })}
     </div>
   );
