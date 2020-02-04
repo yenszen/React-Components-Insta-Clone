d0e0d1291228ec69c62dc02fdf81b68b9e1cbacc
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-02-04 22:28:26 +0800

    completed SearchBar component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import PostsPage from "./components/PostsContainer/PostsPage";
 // import dummydata
+import dummyData from "./dummy-data";
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  const [data, setData] = useState(dummyData);
+  console.log(data);
   return (
     <div className="App">
       {/* Add components here  and pass props where appropriate */}
+      <SearchBar/>
+      <PostsPage data={data} setData={setData} />
     </div>
   );
 };
